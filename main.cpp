6cca79ca7e669e968b70a3ca9b19475b64ee7f7b
ajay0180 <jangraajay489@gmail.com>
2025-03-27 19:00:07 +0530

    final updation1

--- main.cpp
+++ main.cpp
@@ -1,56 +1,54 @@
 #include<iostream>
+#include<iomanip>
 #include<ctime>
 #include<cstdlib>
 
 using namespace std;
 
 int board[4][4];
-            //{down , right , up , left }
+         // down, right, up, left
 int dirLine[]={1,0,-1,0};
 int dirColumn[]={0,1,0,-1};
 
-pair<int,int> generateUnOccupiedPosition() {
-   int occupied=1,line,column;
+pair<int,int>generateUnoccupiedPosition(){
+   int occupied=1;
+   int line,col;
    while(occupied){
-      int line =rand()%4;
-      int column=rand()%4;
-      if(board[line][column]==0) occupied=0;
+      line=rand()%4;
+      col=rand()%4;
+      if(board[line][col]==0) occupied=0;
    }
-   return make_pair(line,column);
-}
-
-void newGame(){
-   for(int i=0;i<4;i++){
-      for(int j=0;j<4;j++){
-         board[i][j]=0;
-      }
-   }
-   pair<int,int>pos=generateUnOccupiedPosition();
-   board[pos.first][pos.second]=2;
-}
-
-void printUI(){
-   for(int i=0;i<4;i++){
-      for(int j=0;j<4;j++){
-         if(board[i][j]==0) cout<<".";
-         else cout<<board[i][j];
-      }
-      cout<<"\n";
-   }
-
-   cout<<"n: new game, w: up, s: down, d: right, a: left, q: quit \n";
-}
-
-bool canDoMove(int line,int column,int nextLine,int nextColumn){
-   if(nextLine<0 || nextColumn<0 || nextLine>=4 || nextColumn>=4 || (board[line][column]!=board[nextLine][nextColumn] && board[nextLine][nextColumn]!=0)){
-      return false;
-   }
-   return true;
+   return make_pair(line,col);
 }
 
 void addPiece(){
-   pair<int,int>pos=generateUnOccupiedPosition();
+   pair<int,int>pos=generateUnoccupiedPosition();
    board[pos.first][pos.second]=2;
+}
+
+void newGame(){
+   for(int i=0;i<4;i++)
+      for(int j=0;j<4;j++)
+         board[i][j]=0;
+   addPiece();
+}
+
+void printUI(){
+   system("clear");
+   for(int i=0;i<4;i++){
+      for(int j=0;j<4;j++){
+         if(board[i][j]==0) cout<<setw(4)<<"*";
+         else cout<<setw(4)<<board[i][j];
+      }
+      cout<<endl;
+   }
+   cout<<"n:new game, w:up, s:down, d:right, a:left, q:quit\n";
+}
+
+bool canDoMove(int line,int column,int nextLine,int nextColumn){
+   if(nextLine<0 || nextColumn<0 || nextLine>=4 || nextColumn>=4 || (board[line][column]!=board[nextLine][nextColumn] && board[nextLine][nextColumn]!=0))
+      return false;
+   return true;
 }
 
 void applyMove(int direction){
@@ -63,30 +61,34 @@ void applyMove(int direction){
       startColumn=3;
       columnStep=-1;
    }
-   int movePossible=0;
-   for(int i=startLine;i>=0 && i<4;i+=lineStep){
-      for(int j=startColumn;j>=0 && j<4;j+=columnStep){
-         int nextI=i+dirLine[direction],nextJ=j+dirColumn[direction];
-         cout<<i<<" "<<j<<" "<<nextI<<" "<<nextJ<<"\n";
-         if(canDoMove(i,j,nextI,nextJ)){
-            board[nextI][nextJ]+=board[i][j];
-            board[i][j]=0;
-            movePossible=1;
+   int movePossible,canAddPiece=0;
+   do{
+      movePossible=0;
+      for(int i=startLine;i>=0 && i<4;i+=lineStep){
+         for(int j=startColumn;j>=0 && j<4;j+=columnStep){
+            int nextI=i+dirLine[direction],nextJ=j+dirColumn[direction];
+            if(board[i][j]!=0 && canDoMove(i,j,nextI,nextJ)==true){
+               board[nextI][nextJ]+=board[i][j];
+               board[i][j]=0;
+               movePossible=canAddPiece=1;
+            }
          }
       }
-   }
-   if(movePossible){
+   }while(movePossible);
+   if(canAddPiece){
       addPiece();
    }
 }
 
 int main(){
    srand(time(0));
+
    char commandToDir[128];
    commandToDir['s']=0;
    commandToDir['d']=1;
    commandToDir['w']=2;
    commandToDir['a']=3;
+
    newGame();
    while(true){
       printUI();
@@ -96,7 +98,6 @@ int main(){
       if(command=='q') break;
       else{
          int currentDirection=commandToDir[command];
-         cout<<currentDirection<<"\n";
          applyMove(currentDirection);
       }
    }
